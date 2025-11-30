c832517d75967ea0d430bab2fa3cbcee688173eb
hadjkouider2 <153817790+hadjkouider2@users.noreply.github.com>
2025-11-30 14:56:36 +0100

    last

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,9 +10,17 @@ const Home = () => {
       <Helmet>
         <title>HOME | Benbouali</title>
         <meta name="description" content="HOME portfolio of Benbouali" />
+        <style type="text/css">{`
+      p {
+            color: red;
+            font-size: 50px;
+        }
+    `}</style>
       </Helmet>
       <Header />
-      <MainContent pageName="Home" designerName="Benbouali" />
+      <p>This is a paragraph with red text and font size 50 px only in Home page.</p>
+      <MainContent pageName="Home" designerName="Benbouali"    />
+      
       <Footer />
     </>
   );
